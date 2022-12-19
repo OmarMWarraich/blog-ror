6b3d45a29951785ed61b9ef9af0d851757a5f91f
OmarMWarraich <earthling986@gmail.com>
2022-12-19 22:34:56 +0500

    feat: Use Strong Parameters to filter params
    Add pvt meth 'article_params' to the bottom of articles_controller.erb and modify create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -20,4 +20,9 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entry
     end
   end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
