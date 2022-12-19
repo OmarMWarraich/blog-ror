8d681930cd325887d9e7181dd250985eb8440afc
OmarMWarraich <earthling986@gmail.com>
2022-12-20 02:52:33 +0500

    feat:Add class_methods namely public_count to module Visible
    Call class method in view and add a select box to forms

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -7,8 +7,14 @@ module Visible
       validates :status, inclusion: { in: VALID_STATUSES }
     end
   
+    class_methods do
+      def public_count
+        where(status: 'public').count
+      end
+    end
+  
     def archived?
       status == 'archived'
     end
   end
-  
+    
