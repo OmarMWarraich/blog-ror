ebbfcc49926d4514d2aff77abc56447315273c6b
OmarMWarraich <earthling986@gmail.com>
2022-12-20 00:21:50 +0500

    feat: Update an Article
    Add Edit and Update Methods, Refactor form into shared view, Modify new.html.erb and create edit.html.erb and add helper link_to edit_article_path(@article) to show.html.erb. PS: Since partials are shared code, article is passed to the partial as local variable rather than specific instance variables set by a controller action precede d by @

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,10 +17,24 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render :new, status: :unprocessable_entry
+      render :new, status: :unprocessable_entity
     end
   end
-  
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
